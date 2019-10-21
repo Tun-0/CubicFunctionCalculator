377be724809bb88dd283b6da4bd92ea03cf4ac04
Tun0 <37117748+tuno609@users.noreply.github.com>
2019-10-21 22:07:17 +0400

    Update Cubic_Calculator.py

--- Cubic_Calculator.py
+++ Cubic_Calculator.py
@@ -1,21 +1,38 @@
-import math  
+import cmath  
+
 print('  ')
 print('  ')
 print('  ')
 print('Cubic function calculator')
 print('                         by tun0')
-a = input('Enter a:')
-b = input('Enter b:')
-c = input('Enter c:')
+
 print('  ')
 print('  ')
 print('  ')
-print("discriminant: {}".format(b*b - 4*a*c))
+
+a = float(input('Enter a:'))
+b = float(input('Enter b:'))
+c = float(input('Enter c:'))
+
+print('  ')
+print('  ')
+print('  ')
+
 d = b*b - 4*a*c
-D = math.sqrt(d)
+
+print("discriminant: {}".format(d))
+
+D = cmath.sqrt(d)
 A = a * 2
+
 print("discriminant root: {}".format(D))
+
 x1 = (b - D)
 x2 = (b + D)
+
+print('  ')
+print('  ')
+print('  ')
+
 print("X1: {}".format(x1/A))
 print("X2: {}".format(x2/A))
